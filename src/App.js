b53d4599dd20b9d9b8e7cbc3b1a642011fcb2956
antguts <antgut035@gmail.com>
2020-07-01 13:51:04 -0400

    JSON item display complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FoodBox from './foodbox/FoodBox.js';
+import foods from './foods.json';
+import './App.css'
+import 'bulma/css/bulma.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+
+class App extends Component {
+  state = {
+    foodList: foods,
+    showForm: false,
+    foodName: '',
+    calories: 0,
+    image: ''
+  }
+  
+  displayFood = () =>{
+    let arr=this.state.foodList.map(food=>{
+      return  <FoodBox 
+                      key={food.name}
+                      name={food.name}  
+                      cal={food.calories}
+                      img={food.image}
+                      />
+    })
+    return arr
+  }
+
+  toggleForm = () =>{
+    this.setState({showForm: !this.state.showForm});
+  };
+
+  addFood=(event)=>{
+    event.preventDefault();
+    let newFoodObj={
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+      quantity: 0
+    }
+
+    let foodListCopy=[...this.state.foodList]
+    foodListCopy.unshift(newFoodObj)
+    
+    this.setState({
+      foodList: foodListCopy
+    })
+  }
+
+
+  handleChange = (event) =>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  displayForm = () =>{
+    return (
+            <form onSubmit={this.addFood}>
+              <label >Food Name: </label>
+              <input onChange={this.handleChange} type='text' name='name'/>
+              <br />
+              <label >Calories: </label>
+              <input onChange={this.handleChange} type='number' name='calories'/>
+              <br />
+              <label >Image URL: </label>
+              <input onChange={this.handleChange} type='text' name='image'/>
+              <br />
+              <input type="submit" />
+            </form>
+    );
+  };
+
+
+  render(){
+    return <div className='App'>
+            <button onClick={this.toggleForm}>Add Food</button>
+            {this.state.showForm? <h1>Form</h1> : this.displayForm()}
+            {this.displayFood()}
+          </div>
+  }//end render
+}//end class
 
 export default App;
